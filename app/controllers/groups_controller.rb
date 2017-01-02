bf0548fdfa1181f8da989d562d23effccf362ab3
“nataliu” <13636424061@163.com>
2017-01-02 21:43:07 +0800

    add validation to title and apply to edit/udpate action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,16 +18,24 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
+    if @group.save
       redirect_to groups_path
+    else
+      render :new
+    end
+
   end
 
   def update
     @group = Group.find(params[:id])
-    @group.update(group_params)
 
+    if @group.update(group_params)
       redirect_to groups_path, notice: "update success"
+    else
+      render :edit
+    end
+
   end
 
   def destroy
